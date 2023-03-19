97af025fb90bb033eceeff532c5287fac65dd404
Oskar Lohse <oskar@baucampus.de>
2023-03-19 16:41:02 +0100

    2

--- resources/views/startseite.blade.php
+++ resources/views/startseite.blade.php
@@ -138,12 +138,14 @@
 
             </section> 
 <!--==================== Wohnimmobilien ====================-->
-<section class="container" id="wohnimmobilien" style="background-color: #E6ECFF; margin-top:3%;">
+<div style="background-color: #E6ECFF; margin-top:3%;">
+<section class="container" id="wohnimmobilien">
     <h3>Wohnimmobilien</h3>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
 
 Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,</p>
 </section>
+</div>
 <!--==================== Gewerbeimmobilien ====================-->
 <section class="container" id="gewerbeimmobilien" style="margin-top:3%;">
     <h3>Gewerbeimmobilien</h3>
